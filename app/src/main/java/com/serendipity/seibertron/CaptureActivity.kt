65f447f6faa1a7518043c62a426c3558748debe7
zhuhuanhuan <zhuhuanhuan@mistong.com>
2019-04-27 16:25:28 +0800

    first commit

---
+++ app/src/main/java/com/serendipity/seibertron/CaptureActivity.kt
@@ -0,0 +1,12 @@
+package com.serendipity.seibertron
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture)
+    }
+}
---
+++ app/src/main/java/com/serendipity/seibertron/ZXingActivity.kt
@@ -0,0 +1,12 @@
+package com.serendipity.seibertron
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ZXingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zxing)
+    }
+}
